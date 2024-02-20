001736074b6af4865a40a139f9c0abfb2fdaafc8
John Hysong <99369137+johnatticus@users.noreply.github.com>
2024-02-20 14:16:14 -0500

    layout started

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,10 +1,12 @@
 <!-- This is blade formatting of the basic php listing below -->
 
+@extends('layout')
+
+@section('content')
+
 <h1>{{$heading}}</h1>
 
-@if(count($listings) == 0)
-    <p>No listings found</p>
-@endif
+@unless(count($listings) == 0)
 
 @foreach($listings as $listing)
     <h2>
@@ -14,6 +16,12 @@
         {{$listing['description']}}
     </p>
 @endforeach
+
+@else
+<p>No listings found</p>
+@endunless
+
+@endsection
 
 <!-- Below is a simple view that will display a list of listings. It will receive a $heading and $listings variable from the controller. The $heading variable will be used to display the title of the page, and the $listings variable will be used to display the list of listings. -->
 
